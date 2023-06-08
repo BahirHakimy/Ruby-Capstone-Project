389f9b2c461f6165e5845a8a33862d45a34d51c6
getssh <gtamrat33@gmail.com>
2023-06-08 23:35:12 +0300

    Add class Book initialization

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,5 +1,12 @@
 require_relative 'item'
 
 class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state, id = nil)
+    super(publish_date, id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
 end
